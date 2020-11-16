28cd5a97cd202c5abe0a80dcc4458d4e8edc7691
Steven Kazmierkiewicz <stevekaz@gmail.com>
2020-11-16 11:07:49 -0500

    set up basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
+import { CareersComponent } from './careers/careers.component';
+import { ContactComponent } from './contact/contact.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    ServicesComponent,
+    CareersComponent,
+    ContactComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
